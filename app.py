720ec663fb6e45a0702cd9da08ced7a747f8df69
J <esteves.j.m@hotmail.com>
2024-06-05 17:26:10 -0300

    agregar node exporter

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
+metrics.info('app_info', 'Application info', version='1.0.3')
 # Endpoint que devuelve "Hola Mundo"
 @app.route('/')
+@metrics.counter('hello_world_requests', 'Number of requests to hello_world endpoint')
 def hello_world():
     return 'Hola Mundo!'
 
 # Endpoint que devuelve un error
 @app.route('/error')
+@metrics.counter('error_requests', 'Number of requests to error endpoint')
 def error():
     # Simulamos un error con un código de estado 500 (Internal Server Error)
     return jsonify({'error': 'Se ha producido un error'}), 500
 
 # Endpoint que indica que el sitio está en mantenimiento
 @app.route('/mantenimiento')
+@metrics.counter('maintenance_requests', 'Number of requests to maintenance endpoint')
 def mantenimiento():
     # Simulamos un mensaje de mantenimiento con un código de estado 503 (Service Unavailable)
     return jsonify({'mensaje': 'El sitio está en mantenimiento, vuelva más tarde'}), 503
